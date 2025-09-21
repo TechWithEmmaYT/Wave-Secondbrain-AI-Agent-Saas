24c471f8476e2c9637ac2794c1bf2d2b873e5043
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2025-09-21 18:12:54 +0100

    First commit

--- app/(routes)/(dashboard)/_common/header.tsx
+++ app/(routes)/(dashboard)/_common/header.tsx
@@ -23,7 +23,7 @@ const Header = ({ title, showActions = false }: Props) => {
   };
   return (
     <header
-      className="fixed w-full top-0 inset-y-0 z-[9] h-[40px]
+      className="fixed top-0 inset-0 z-[9] h-[40px]
       flex items-center px-2  md:px-8 py-1 bg-background/20 backdrop-blur-sm"
     >
       {(!open || isMobile) && <SidebarTrigger className="h-10" />}
@@ -32,7 +32,7 @@ const Header = ({ title, showActions = false }: Props) => {
         <div
           className={cn(
             "pt-5",
-            open && "w-full !max-w-full !lg:p-[10px_0_0_250px]"
+            open && "w-full !max-w-full lg:p-[10px_0_0_250px]"
           )}
         >
           <h2 className="text-xl lg:text-2xl font-semibold">{title}</h2>
@@ -43,7 +43,7 @@ const Header = ({ title, showActions = false }: Props) => {
         <div
           className={cn(
             "w-full flex items-center justify-between",
-            open && "w-full !max-w-full !lg:p-[0_0_0_250px]"
+            open && "w-full !max-w-full lg:!p-[0_0_0_250px]"
           )}
         >
           <h2>{title}</h2>
